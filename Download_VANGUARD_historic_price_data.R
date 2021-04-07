4ec84732170d9172d77c5a1a80ca0e34d64241f0
Gabriel Pérez Murillo <39721856+gperez1404@users.noreply.github.com>
2021-04-07 12:00:08 +1000

    Add files via upload

---
+++ Download_VANGUARD_historic_price_data.R
@@ -0,0 +1,43 @@
+
+# Scraping and Analyzing Vangaurd ETFs :
+
+# this scrip downlaod historical data of buy/sell prices for a especific product
+
+# references:
+
+# https://rstudio-pubs-static.s3.amazonaws.com/392483_2e403b6b7f4e473b8e87461aa9674599.html
+
+
+
+library(httr)
+library(xml2)
+
+#  packages for scraping JAvaScrip libraries: 
+library(rvest)
+library(stringr)
+library(tidyverse)
+library(purrr)
+library(here)
+library(beepr)
+library(DT)
+
+
+target_link <- "https://www.vanguard.com.au/personal/products/en/detail/8101/prices-and-distributions"
+url <- GET(target_link)
+
+
+# Function for scraping from a URL
+js_scrape <- function(url, 
+                      js_path = "scrape.js", 
+                      phantompath = "/usr/local/Cellar/phantomjs/2.1.1/bin/phantomjs"){
+  
+  # Replace url in scrape.js
+  lines <- readLines(js_path)
+  lines[1] <- paste0("var url ='", url ,"';")
+  writeLines(lines, js_path)
+  
+  # Run from command line
+  command <- paste(phantompath, js_path, sep = " ")
+  system(command)
+  
+}
